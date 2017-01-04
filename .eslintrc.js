0654ddd01c900e4318bb9ced748bede3e1140d07
Kagami <moe.pascal@gmail.com>
2017-01-04 19:48:30 +0800

    add eslintc

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  'installedESLint': true,
+  'parser': 'babel-eslint',
+  'plugins': [
+    'import',
+    'react',
+  ],
+  'globals': {
+    'html2canvas': false,
+    "window": true,
+  },
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': ['warn', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': ['warn', {'allow': ['warn', 'error']}],
+    'no-var': 'error',
+    'no-unused-vars': ['warn', {'args': 'none'}],
+    'semi': ['error', 'never'],
+    'unicode-bom': 'error',
+    'react/prop-types': 'off',
+    'import/no-unresolved': [2, { ignore: ['views/.*'] }],
+    "react/jsx-indent": [1, 2],
+    "react/jsx-indent-props": [1, 2],
+    "react/jsx-closing-bracket-location": [1, 'tag-aligned'],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.es', '.coffee', '.cjsx'],
+        'paths': [__dirname],
+      },
+    },
+    'import/core-modules': [
+      'electron',
+      'react',
+      'react-dom',
+      'react-redux',
+      'redux-observers',
+      'reselect',
+      'react-bootstrap',
+      'react-fontawesome',
+      'path-extra',
+      'fs-extra',
+      'lodash',
+      'cson',
+    ],
+  },
+}
