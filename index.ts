9d5c7e53064a86f538ab062ec6bccd9216a602f2
Kagami <moe.pascal@gmail.com>
2019-09-07 15:56:53 +0800

    feat: remove legacy window positioning code

--- index.ts
+++ index.ts
@@ -1,38 +1,8 @@
-import { Display, remote } from 'electron'
-
-const screen = remote.screen
-const { workArea } = screen.getPrimaryDisplay()
-let { x, y, width, height } = global.config.get(
-  'plugin.ShipInfo.bounds',
-  workArea,
-)
-const validate = (n: number, min: number, range: number): boolean =>
-  n != null && n >= min && n < min + range
-
-const withinDisplay = (d: Display): boolean => {
-  const wa = d.workArea
-  return validate(x, wa.x, wa.width) && validate(y, wa.y, wa.height)
-}
-
-if (!screen.getAllDisplays().some(withinDisplay)) {
-  ;({ x, y } = workArea)
-}
-if (width == null) {
-  ;({ width } = workArea)
-}
-if (height == null) {
-  ;({ height } = workArea)
-}
-
-export const windowOptions = {
-  height,
-  width,
-  x,
-  y,
-}
 export { reducer, reactClass } from './views'
 
 export const pluginDidLoad = (): void => {
   global.config.set('plugin.ShipInfo.shipTypeChecked')
+  global.config.set('plugin.ShipInfo.bounds')
 }
+
 export const windowMode = true
