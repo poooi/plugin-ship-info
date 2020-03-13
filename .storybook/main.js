a6e5e78674b9763dcd8242b9653de21df3cd5b6d
Kagami <moe.pascal@gmail.com>
2020-03-14 02:49:43 +0800

    feat: add storybook for preview

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: ['../views/**/*.stories.tsx'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          require.resolve('@babel/preset-env'),
+          require.resolve('@babel/preset-react'),
+          require.resolve('@babel/preset-typescript'),
+        ],
+        plugins: [],
+      },
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
